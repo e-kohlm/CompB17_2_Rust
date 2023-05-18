a63a4ae46d1af9305839e9532ae415d18a4b7f0a
kohlmane <elke.kohlmann@informatik.hu-berlin.de>
2023-05-18 19:23:21 +0200

    Aufgabe 1 - test passed

--- src/lexer/c1.rs
+++ src/lexer/c1.rs
@@ -2,10 +2,95 @@ use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
 pub enum C1Token {
-    // TODO: Define variants and their token/regex
+    // Variants and their token/regex
+    //Key Words
+    #[token("bool")]
+    KwBoolean,
+    #[token("do")]
+    KwDo,
+    #[token("else")]
+    KwElse,
+    #[token("float")]
+    KwFloat,
+    #[token("for")]
+    KwFor,
+    #[token("if")]
+    KwIf,
+    #[token("int")]
+    KwInt,
+    #[token("printf")]
+    KwPrintf,
+    #[token("return")]
+    KwReturn,
+    #[token("void")]
+    KwVoid,
+    #[token("while")]
+    KwWhile,
 
-    // Logos requires one token variant to handle errors,
-    // it can be named anything you wish.
+    //Operators
+    #[token("+")]
+    Plus,
+    #[token("-")]
+    Minus,
+    #[token("*")]
+    Asterisk,
+    #[token("/")]
+    Slash,
+    #[token("=")]
+    Assign,
+    #[token("==")]
+    Eq,
+    #[token("!=")]
+    Neq,
+    #[token("<")]
+    Lss,
+    #[token(">")]
+    Grt,
+    #[token("<=")]
+    Leq,
+    #[token(">=")]
+    Geq,
+    #[token("&&")]
+    And,
+    #[token("||")]
+    Or,
+
+    //Other
+    #[token(",")]
+    Comma,
+    #[token(";")]
+    Semicolon,
+    #[token("(")]
+    LParen,
+    #[token(")")]
+    RParen,
+    #[token("{")]
+    LBrace,
+    #[token("}")]
+    RBrace,
+
+    //Term variants
+    #[regex(r"\d+")]
+    ConstInt,
+    #[regex(r"\d*(\.\d+)?([eE][-+]?\d+)?")]
+    ConstFloat,
+    #[token("true")]
+    #[token("false")]
+    ConstBoolean,
+    #[regex(r#""([^"\\]|\\.)*""#)]
+    ConstString,
+    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*")]
+    Id,
+
+    //Whitespaces, EOF, Tabs, and Comments to be ignored
+    #[regex(r" ", logos::skip)]
+    #[regex(r"\n", logos::skip)]
+    #[regex(r"\t", logos::skip)]
+    #[regex(r"//.*", logos::skip)]
+    #[regex(r"/\*([^*]|\*[^/])*\*/", logos::skip)]
+    Ignored,
+
+    // Logos requires one token variant to handle errors
     #[error]
     Error,
 }
