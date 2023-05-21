5246e3c1d723dc02a6d2d29e2579ab9ce5f843ec
kohlmane <elke.kohlmann@informatik.hu-berlin.de>
2023-05-21 21:20:06 +0200

    Aufgabe 1 & 2 tests passed

--- src/lexer/url.rs
+++ src/lexer/url.rs
@@ -1,4 +1,4 @@
-use logos::{Lexer, Logos, Source};
+use logos::{Lexer, Logos};
 use std::fmt::{Display, Formatter};
 
 /// Tuple struct for link URLs
@@ -26,10 +26,12 @@ impl Display for LinkText {
 /// Token enum for capturing of link URLs and Texts
 #[derive(Logos, Debug, PartialEq)]
 pub enum URLToken {
-    // TODO: Capture link definitions
+    // Capture link definitions
+    #[regex(r#"<a [^h]*href="[^"]*"[^>]*>[^<]*</a[ \s]*>"#, extract_link_info)]
     Link((LinkUrl, LinkText)),
 
-    // TODO: Ignore all characters that do not belong to a link definition
+    // Ignore all characters that do not belong to a link definition
+    #[regex(r"<[^>]*>|[^<]*", logos::skip)]
     Ignored,
 
     // Catch any error
@@ -39,6 +41,21 @@ pub enum URLToken {
 
 /// Extracts the URL and text from a string that matched a Link token
 fn extract_link_info(lex: &mut Lexer<URLToken>) -> (LinkUrl, LinkText) {
-    // TODO: Implement extraction from link definition
-    todo!()
+    // Implement extraction from link definition
+    let sliced_string = lex.slice();
+
+    // Search for href and extract starting index
+    let start = sliced_string.find(r#"href=""#).unwrap() + 6;
+    // Search for "" and extract index
+    let end = start + sliced_string[start..].find(r#"""#).unwrap();
+
+    // Same for linktext
+    let link_text_start = sliced_string.find(">").unwrap() + 1;
+    let link_text_end = link_text_start + sliced_string[link_text_start..].find("<").unwrap();
+
+    // From index create string of URL and linktext
+    let link_url = sliced_string[start..end].to_string();
+    let link_text = sliced_string[link_text_start..link_text_end].to_string();
+
+    return (LinkUrl(link_url), LinkText(link_text));
 }
